b69df71355ca7abd8e3c00adf1cd5602e5c03f95
thehienvnag <thehienvnag@gmail.com>
2020-04-17 08:31:57 +0700

    5th push: red color

--- test/tdd/MyToyTest.java
+++ test/tdd/MyToyTest.java
@@ -5,10 +5,8 @@
  */
 package tdd;
 
-import com.sun.corba.se.impl.io.TypeMismatchException;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import org.omg.IOP.CodecPackage.TypeMismatch;
 import static util.MyToys.cF;
 /**
  *
@@ -18,10 +16,23 @@ public class MyToyTest {
     public MyToyTest() {
         //
     }    
+    
     @Test
-    public void testSuccessfulCases(){
+    public void testSuccessfulGreater10Cases(){
         //actual + call method
-        assertEquals(1, cF(0));
+        assertEquals(3_628_800, cF(10)); //xay ra 2 tinh huong:
+                                    //xu ly sai
+                                    //ki vong sai
+                                    //co do, kiem tra code di
+                                    //Ngoai le Exception la 1 thu ko phai value de uoc luong
+                                    //Vay ko the assert duoc vi no can value de so sanh
+                                    //Xai them 1 ky thuat bat ngoai le. Coi ngoai xay ra co giong nhu ky vong ko       
+    }
+    
+    @Test
+    public void testSuccessfulLower10Cases(){
+        //actual + call method
+        assertEquals(10, cF(0));
         assertEquals(2, cF(2));
         assertEquals(6, cF(3));
         assertEquals(24, cF(4));
@@ -35,9 +46,16 @@ public class MyToyTest {
                                     //Xai them 1 ky thuat bat ngoai le. Coi ngoai xay ra co giong nhu ky vong ko       
     }
     @Test(expected = IllegalArgumentException.class)
-    public void testExceptionCases(){
+    public void testExceptionNegativeCases(){
         //ko co value de assert do ham cF() nem ra ngoai le
         cF(-5);
+        cF(-100);
+        cF(-1);
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void testExceptionUpperBoundCases(){
+        //ko co value de assert do ham cF() nem ra ngoai le
+        cF(16);
         cF(100);
     }
 }
